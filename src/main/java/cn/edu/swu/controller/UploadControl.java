1cfd04044859a2b76af2814cfc06b72c6d0c9308
BiCCCCCC <994303882@qq.com>
2022-07-20 10:03:01 +0800

    更新

--- src/main/java/cn/edu/swu/controller/UploadControl.java
+++ src/main/java/cn/edu/swu/controller/UploadControl.java
@@ -1,20 +1,20 @@
 package cn.edu.swu.controller;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
+
 import java.io.File;
 import java.io.IOException;
 
-
 @Controller
-@RequestMapping("/user")
 public class UploadControl {
 
-//上传
-    @RequestMapping(value = "/quick20")
+    //上传
+    @RequestMapping(value = "/upload")
     @ResponseBody
-    public void save20(String username, MultipartFile[] uploadFiles) throws IOException {
+    public void upload(String username, MultipartFile[] uploadFiles) throws IOException {
         System.out.println(username);
         System.out.println(uploadFiles[0].getOriginalFilename());
         File file = new File("E:\\springtmp\\" + uploadFiles[0].getOriginalFilename());
